6cbef4cd10b65994c5ccb468c981a4201f2adeda
Chickenwingcandy <chickenwingcandy23@gmail.com>
2021-03-19 16:12:46 -0400

    Added testing and set up a skeleton test.

--- src/test/java/controller/RecipesControllerTest.java
+++ src/test/java/controller/RecipesControllerTest.java
@@ -2,10 +2,22 @@ package controller;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.*;
 
+
+@SpringBootTest
 public class RecipesControllerTest {
 
+    @Autowired
+    private RecipesController recipesController;
+
+    @Test
+    public void contextLoads(){
+
+    }
 
 }
